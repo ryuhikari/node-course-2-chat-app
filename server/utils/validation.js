10198fccf4b09ea6cd6692ffd2097681684e1c62
Ryuhikari <fernandovillab@gmail.com>
2017-05-22 16:48:18 +0200

    Send messages to only people in same room

--- server/utils/validation.js
+++ server/utils/validation.js
@@ -1,11 +1,17 @@
-var isRealString = function(str) {
-    if (typeof str === 'string') {
-        str = str.trim();
-        if ( !( /[^\w\s]+/ig.test(str) || str.length === 0 ) ) {
-            return true;
-        }
-    }
-    return false;
+var isRealString = (str) => {
+  return typeof str === 'string' && str.trim().length > 0;
 };
 
 module.exports = {isRealString};
+
+// var isRealString = function(str) {
+//     if (typeof str === 'string') {
+//         str = str.trim();
+//         if ( !( /[^\w\s]+/ig.test(str) || str.length === 0 ) ) {
+//             return true;
+//         }
+//     }
+//     return false;
+// };
+//
+// module.exports = {isRealString};
